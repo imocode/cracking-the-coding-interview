1f87a27e56a25df0ffe9157daeab12a618a55d33
Omar Guntaue <oguntaue@amazon.es>
2023-02-18 10:57:42 +0100

    C1.9 and cleanup

--- C1.8/Program.cs
+++ C1.8/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using static System.Net.WebRequestMethods;
 /**
  * Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
@@ -33,27 +34,65 @@ int[,] expected = new int[3, 4] {
 };
 
 int[,] result = ZeroMatrix(input);
-System.Diagnostics.Debug.Assert(result.Equals(expected));
+ArrayComparer comparer = new ArrayComparer();
+
+System.Diagnostics.Debug.Assert(comparer.Equals(result, expected));
 
 static int[,] ZeroMatrix(int[,] input)
 {
     int[] rows = new int[input.GetLength(0)];
     int[] cols = new int[input.GetLength(1)];
 
-    for(int row = 0; row < input.GetLength(0); row++ ) { 
-        for(int col = 0; col < input.GetLength(1); col++) {
-            if (input[row,col] == 0)
+    for (int row = 0; row < input.GetLength(0); row++)
+    {
+        for (int col = 0; col < input.GetLength(1); col++)
+        {
+            if (input[row, col] == 0)
             {
                 rows[row] = 1;
-                cols[col] = 1;    
-	        }
-	    }
+                cols[col] = 1;
+            }
+        }
     }
-    for(int row = 0; row < input.GetLength(0); row++ ) { 
-        for(int col = 0; col < input.GetLength(1); col++) {
+    for (int row = 0; row < input.GetLength(0); row++)
+    {
+        for (int col = 0; col < input.GetLength(1); col++)
+        {
             if (rows[row] == 1 || cols[col] == 1)
                 input[row, col] = 0;
-	    }
+        }
     }
     return input;
+}
+
+class ArrayComparer : IEqualityComparer<int[,]>
+{
+    public bool Equals(int[,]? x, int[,]? y)
+    {
+        if (x.GetLength(0) != y.GetLength(0) || x.GetLength(1) != y.GetLength(1))
+            return false;
+
+        for (int i = 0; i < x.GetLength(0); i++)
+        {
+            for (int j = 0; j < x.GetLength(1); j++)
+            {
+                if (x[i, j] != y[i, j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public int GetHashCode([DisallowNull] int[,] obj)
+    {
+        int code = 0;
+        for (int i = 0; i < obj.GetLength(0); i++)
+        {
+            for (int j = 0; j < obj.GetLength(1); j++)
+            {
+                code = code + obj[i, j];
+            }
+        }
+        return code.GetHashCode();
+    }
 }
---
+++ C1.9/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+StringRotation: Assume you have a method i5Sub5tring which checks if one word is 
+a substring of another. 
+
+Given two strings, S1 and S2, write code to check if S2 is a rotation of S1 
+using only one call to i5Sub5tring (e.g., "waterbottle" is a rotation of" 
+erbottlewat").
+
+waterbottle wat-erbottle
+erbottlewat erbottle-wat
+
+w aterbottle -> aterbottlew
+wa terbottle
+wat erbottle
+wate rbottle
+water bottle
+waterb ottle
+waterbo ttle
+waterbot tle
+waterbott le
+waterbottl e
+
+Q: is waterbottle rotation of waterbottle?
+
+The first thing that comes to mind is to have a string, doble size of the 
+original, duplicating all its content. In this case, the new string would be 
+"waterbottlewaterbottle". Then, the isSubstring can be called only one. Time
+complexity should be constant, but space is taking twice the size of the string.
+
+
+*/
+
+using System.Diagnostics;
+
+Debug.Assert(IsStringRotation("waterbottle", "terbottlewa"));
+Debug.Assert(!IsStringRotation("waterbottle", "terbottlew"));
+
+
+static bool IsStringRotation(string s1, string s2) {
+    if (s1.Length != s2.Length)
+        return false;
+
+    string doubleString = s1 + s1;
+
+    if (doubleString.Contains(s2))
+        return true;
+    return false;
+}
